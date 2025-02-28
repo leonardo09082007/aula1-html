24e84b22fa9d61179445a01f2dba2150f535cdc3
leonardo09082007 <163143692+leonardo09082007@users.noreply.github.com>
2025-02-28 11:08:57 -0300

    Create condicoes.js

---
+++ condicoes.js
@@ -0,0 +1,13 @@
+// condições 
+
+let idade = 19
+let possuiCNH = true
+if (idade >= 18) {
+    if(possuiCNH == true){
+    console.log("Vocẽ pode dirigir!")
+} else {
+    console.log("vocẽ pode tirar CNH")
+   }
+} else {
+    console.log("Não pode")
+}
