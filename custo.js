e49fb9fac278b10a2662f939fa39d1348f8d7442
leonardo09082007 <163143692+leonardo09082007@users.noreply.github.com>
2025-04-29 11:33:21 -0300

    Create custo.js

---
+++ custo.js
@@ -0,0 +1,20 @@
+let custos = [800,1000,300,500];
+let contador = 0;
+let somatorio = 0;
+while(contador<custos.length){
+somatorio=somatorio+custos[contador];
+contador=contador+1;
+}
+console.log("O seu custo mensal é " + somatorio);
+
+let ganhos = [350,5200,922,551];
+let contadorsalario = 0;
+let somatoriosalario = 0;
+while(contadorsalario<ganhos.length){
+somatoriosalario=somatoriosalario+ganhos[contadorsalario];
+contadorsalario=contadorsalario+1;
+}
+console.log("O seu ganho mensal é " + somatoriosalario);
+
+let diferenca = somatoriosalarios - somatorioCustos;
+console.log ("O seu saldo mensal é R$ " + diferenca);
